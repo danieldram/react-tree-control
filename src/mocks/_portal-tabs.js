cc25efa49835f769e56a46d5bac82467ee1f11f0
daniel ram <danieldram@gmail.com>
2017-05-07 19:24:41 -0700

    update to mocks

--- src/mocks/_portal-tabs.js
+++ src/mocks/_portal-tabs.js
@@ -54,104 +54,104 @@ export const PortalTabs =  [{
                         "CheckedState": 1,
                         "ChildTabs": []
                     }
-                    //,{
-                    //     "Name": "Inner Inner Sib 2",
-                    //     "TabId": "2012",
-                    //     "ImageUrl": "/DesktopModules/Admin/Tabs/images/Icon_Home.png",
-                    //     "Tooltip": "Homepage of the site",
-                    //     "ParentTabId": '201',
-                    //     "HasChildren": false,
-                    //     "IsOpen": false,
-                    //     "Selectable": true,
-                    //     "CheckedState": 1,
-                    //     "ChildTabs": []
-                    // }
+                    ,{
+                        "Name": "Inner Inner Sib 2",
+                        "TabId": "2012",
+                        "ImageUrl": "/DesktopModules/Admin/Tabs/images/Icon_Home.png",
+                        "Tooltip": "Homepage of the site",
+                        "ParentTabId": '201',
+                        "HasChildren": false,
+                        "IsOpen": false,
+                        "Selectable": true,
+                        "CheckedState": 1,
+                        "ChildTabs": []
+                    }
                   ]
               }]
         }
-        // ,{
-        //     "Name": "Activity Feed",
-        //     "TabId": "21",
-        //     "ImageUrl": "/DesktopModules/Admin/Tabs/images/Icon_Everyone.png",
-        //     "Tooltip": "Page is visible to everyone",
-        //     "ParentTabId": "-1",
-        //     "HasChildren": true,
-        //     "IsOpen": false,
-        //     "Selectable": true,
-        //     "CheckedState": 2,
-        //     "ChildTabs": [
-        //       {
-        //           "Name": "Inner Child 2",
-        //           "TabId": "211",
-        //           "ImageUrl": "/DesktopModules/Admin/Tabs/images/Icon_Home.png",
-        //           "Tooltip": "Homepage of the site",
-        //           "ParentTabId": "21",
-        //           "HasChildren": true,
-        //           "IsOpen": false,
-        //           "Selectable": true,
-        //           "CheckedState": 1,
-        //           "ChildTabs": [
-        //             {
-        //                 "Name": "Inner Inner Child 2",
-        //                 "TabId": "2111",
-        //                 "ImageUrl": "/DesktopModules/Admin/Tabs/images/Icon_Home.png",
-        //                 "Tooltip": "Homepage of the site",
-        //                 "ParentTabId": '211',
-        //                 "HasChildren": false,
-        //                 "IsOpen": false,
-        //                 "Selectable": true,
-        //                 "CheckedState": 1,
-        //                 "ChildTabs": []
-        //             }
-        //           ]
-        //       },{
-        //           "Name": "Inner Child Sibling",
-        //           "TabId": "212",
-        //           "ImageUrl": "/DesktopModules/Admin/Tabs/images/Icon_Home.png",
-        //           "Tooltip": "Homepage of the site",
-        //           "ParentTabId": "21",
-        //           "HasChildren": true,
-        //           "IsOpen": false,
-        //           "Selectable": true,
-        //           "CheckedState": 2,
-        //           "ChildTabs": [
-        //             {
-        //                 "Name": "Inner Inner Child 2",
-        //                 "TabId": "2121",
-        //                 "ImageUrl": "/DesktopModules/Admin/Tabs/images/Icon_Home.png",
-        //                 "Tooltip": "Homepage of the site",
-        //                 "ParentTabId": '212',
-        //                 "HasChildren": false,
-        //                 "IsOpen": false,
-        //                 "Selectable": true,
-        //                 "CheckedState": 1,
-        //                 "ChildTabs": []
-        //             }
-        //           ]
-        //       }
-        //     ]
-        // },{
-        //     "Name": "Search Results",
-        //     "TabId": "22",
-        //     "ImageUrl": "/DesktopModules/Admin/Tabs/images/Icon_Everyone.png",
-        //     "Tooltip": "Page is visible to everyone",
-        //     "ParentTabId": "-1",
-        //     "HasChildren": false,
-        //     "IsOpen": false,
-        //     "Selectable": true,
-        //     "CheckedState": 1,
-        //     "ChildTabs": []
-        // },{
-        //     "Name": "404 Error Page",
-        //     "TabId": "23",
-        //     "ImageUrl": "/DesktopModules/Admin/Tabs/images/Icon_Everyone.png",
-        //     "Tooltip": "Page is visible to everyone",
-        //     "ParentTabId": "-1",
-        //     "HasChildren": false,
-        //     "IsOpen": false,
-        //     "Selectable": true,
-        //     "CheckedState": 1,
-        //     "ChildTabs": []
-        // }
+        ,{
+            "Name": "Activity Feed",
+            "TabId": "21",
+            "ImageUrl": "/DesktopModules/Admin/Tabs/images/Icon_Everyone.png",
+            "Tooltip": "Page is visible to everyone",
+            "ParentTabId": "-1",
+            "HasChildren": true,
+            "IsOpen": false,
+            "Selectable": true,
+            "CheckedState": 2,
+            "ChildTabs": [
+              {
+                  "Name": "Inner Child 2",
+                  "TabId": "211",
+                  "ImageUrl": "/DesktopModules/Admin/Tabs/images/Icon_Home.png",
+                  "Tooltip": "Homepage of the site",
+                  "ParentTabId": "21",
+                  "HasChildren": true,
+                  "IsOpen": false,
+                  "Selectable": true,
+                  "CheckedState": 1,
+                  "ChildTabs": [
+                    {
+                        "Name": "Inner Inner Child 2",
+                        "TabId": "2111",
+                        "ImageUrl": "/DesktopModules/Admin/Tabs/images/Icon_Home.png",
+                        "Tooltip": "Homepage of the site",
+                        "ParentTabId": '211',
+                        "HasChildren": false,
+                        "IsOpen": false,
+                        "Selectable": true,
+                        "CheckedState": 1,
+                        "ChildTabs": []
+                    }
+                  ]
+              },{
+                  "Name": "Inner Child Sibling",
+                  "TabId": "212",
+                  "ImageUrl": "/DesktopModules/Admin/Tabs/images/Icon_Home.png",
+                  "Tooltip": "Homepage of the site",
+                  "ParentTabId": "21",
+                  "HasChildren": true,
+                  "IsOpen": false,
+                  "Selectable": true,
+                  "CheckedState": 2,
+                  "ChildTabs": [
+                    {
+                        "Name": "Inner Inner Child 2",
+                        "TabId": "2121",
+                        "ImageUrl": "/DesktopModules/Admin/Tabs/images/Icon_Home.png",
+                        "Tooltip": "Homepage of the site",
+                        "ParentTabId": '212',
+                        "HasChildren": false,
+                        "IsOpen": false,
+                        "Selectable": true,
+                        "CheckedState": 1,
+                        "ChildTabs": []
+                    }
+                  ]
+              }
+            ]
+        },{
+            "Name": "Search Results",
+            "TabId": "22",
+            "ImageUrl": "/DesktopModules/Admin/Tabs/images/Icon_Everyone.png",
+            "Tooltip": "Page is visible to everyone",
+            "ParentTabId": "-1",
+            "HasChildren": false,
+            "IsOpen": false,
+            "Selectable": true,
+            "CheckedState": 1,
+            "ChildTabs": []
+        },{
+            "Name": "404 Error Page",
+            "TabId": "23",
+            "ImageUrl": "/DesktopModules/Admin/Tabs/images/Icon_Everyone.png",
+            "Tooltip": "Page is visible to everyone",
+            "ParentTabId": "-1",
+            "HasChildren": false,
+            "IsOpen": false,
+            "Selectable": true,
+            "CheckedState": 1,
+            "ChildTabs": []
+        }
       ]
     }]
